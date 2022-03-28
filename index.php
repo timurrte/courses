0637e356275fe070c13a92fc8bb2107f519f5df5
timurrte <timurnikolaus@gmail.com>
2022-03-28 21:01:47 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MyClass Class Doc Comment
+ *
+ * @category Learn
+ * @package  Tutorial
+ * @author   timurrte <timurnikolaus@gmail.com> 
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     
+ *
+ */
+class MyClass{
+}
